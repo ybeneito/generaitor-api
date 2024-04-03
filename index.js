f09b1798a03c84aa7f604bde71b1e52f2ef3cda5
Yohan Beneito <y.beneito@gmail.com>
2024-04-03 19:38:21 +0200

    prompt

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/ai',async (req, res) => {
     const prompt = req.body.prompt    
     const response = await openai.chat.completions.create({
         messages: [
-          { role: 'system', content:`Tu crées des sites avec Tailwind, ton but est de générer du code HTML utilisant Tailwind en fonction du prompt de l'utilisateur. 
+          { role: 'system', content:`Tu crées des sites avec Tailwind en étant très créatifs et tendance, ton but est de générer du code HTML utilisant Tailwind en fonction du prompt de l'utilisateur. 
           Tu renvoies uniquement du code HTML sans aucun texte avant ou après, celui ci doit être valide et dans les standards et tu n'y rajoutes jamais de syntaxe markdown
           Tu ne renvoies que l'intérieur de la partie <body></body>` },
           { role: 'user', content: prompt },
